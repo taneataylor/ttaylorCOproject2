d590553659b9cac06bd265b2c66176033137861b
taneataylor <taylor_tanea@yahoo.com>
2018-11-07 22:32:09 -0500

    Case of larger string
    Beginning implementation for the case that if the string has more than 4 characters

--- project2.s
+++ project2.s
@@ -1,6 +1,12 @@
-  .text 
+.data
+  .data
+string: .asciiz "Hello"
+errorMsg: .asciiz "Input is too long."
+
+    .text 
   .globl main
-  
 main:
   li $a0, 0  # first argument
   syscall
+  
+  la $a0, string # loads strings address
