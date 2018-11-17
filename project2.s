b3f90c59938fc13e79f8262a564042f941c9038c
taneataylor <taylor_tanea@yahoo.com>
2018-11-16 21:52:34 -0500

    First half of bigger commit/program updates
    In this half code has main function that loads user input and also has the function that deletes spaces to the spaces on left side

--- project2.s
+++ project2.s
@@ -23,8 +23,22 @@ error_long_input:  # long input case
   syscall
   j exit
   
-error_invalid_input:
+error_invalid_input:  # invalid input case
   la $a0, invalidMsg  # loads invalidMsg string to register
   li $v0, 4
   syscall
   j exit
+
+main:
+  li $v0, 8  # reads the string in
+  la $a0, userMsg
+  li $a1, 250 # loads 250 into a1 register
+  syscall
+  
+delete_left_spaces:
+  li $t3, 32  # $t3 = 32(space)
+  lb $t4, 0($a0)
+  beq $t3, $t4, delete_first_char  # branch to delete_first_char if $t3 == $t4
+  move $t4, $a0  # copies value from $t4 register to $a0
+  j input_length  # jumps to input_length function
+  
