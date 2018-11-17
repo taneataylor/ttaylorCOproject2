5feb98ab0f75e8954de05f27dc10b9c70065b90e
taneataylor <taylor_tanea@yahoo.com>
2018-11-16 22:57:21 -0500

    Updates
    beginning of conversion_prep function

--- project2.s
+++ project2.s
@@ -88,4 +88,8 @@ move_char:  # function that moves char in string forward
   addi $a0, $a0, 1
   j check_strings # jumps to check_strings function
   
-  
+conversion_prep:
+  move $a0, $t4
+  addi $t7, $t7, 0
+  add $s0, $s0, $t0
+  addi $s0, $s0, -1
