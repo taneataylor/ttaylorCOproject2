f8f34cc32b648c5c9a520a19c0944f988fd62f87
taneataylor <taylor_tanea@yahoo.com>
2018-11-16 20:00:35 -0500

    Updates: 1 new case completed
    Completed long input case. When user input is longer than expected, prints error message "Input is too long.".

--- project2.s
+++ project2.s
@@ -1,20 +1,24 @@
-# N = 
+# N = 31
 .data
   # string: .asciiz "Hello" 
   emptyMsg: .asciiz "Input is empty."
   errorMsg: .asciiz "Input is too long."
   userMsg: .space 500
-  invalidMsg: .asciiz "Invalid base-N number."
+  invalidMsg: .asciiz "Invalid base-31 number."
 
 .text
 exit:  # exit function
   li $v0, 10 # loads exit/10
   syscall
   
-error_empty_input:  #
+error_empty_input:  # empty case
   la $a0, emptyMsg  # load emptyMsg string into register $a0  
   li $v0, 4  # print string
   syscall
   j exit  # jumps to exit function
   
-  
+error_long_input:  # long input case
+  la $a0, errorMsg  # load errorMsg string into register
+  li $v0, 4
+  syscall
+  j exit
